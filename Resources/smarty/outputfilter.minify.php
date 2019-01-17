81b38a2e7eac017883bee35410049b2835ff5826
Soner Sayakci <s.sayakci@shopware.com>
2019-01-17 13:46:31 +0100

    Fix javascript

--- Resources/smarty/outputfilter.minify.php
+++ Resources/smarty/outputfilter.minify.php
@@ -5,6 +5,7 @@ function smarty_outputfilter_minify($output)
     if (strpos($output, '<head>') == -1) {
         return $output;
     }
+
     /*
      * Uses Alan Moore's regexp:
      * http://stackoverflow.com/questions/5312349/minifying-final-html-output-using-regular-expressions-with-codeigniter
@@ -12,5 +13,5 @@ function smarty_outputfilter_minify($output)
      * the following code to minify the HTML output
      * (it leaves whitespace within `<pre>` and `<textarea>` tags untouched)
      */
-    return preg_replace('#(?ix)(?>[^\S ]\s*|\s{2,})(?=(?:(?:[^<]++|<(?!/?(?:textarea|pre)\b))*+)(?:<(?>textarea|pre)\b|\z))#', ' ', $output);
+    return preg_replace('#(?ix)(?>[^\S ]\s*|\s{2,})(?=(?:(?:[^<]++|<(?!/?(?:textarea|pre|script)\b))*+)(?:<(?>textarea|pre|script)\b|\z))#', ' ', $output);
 }
