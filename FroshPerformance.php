4212543101103b81a9d86cfb5b6f4d28cf3cd22d
tinect <s.koenig@tinect.de>
2019-01-29 10:28:32 +0100

    message on install on not compatible versions - against Wutbürger

--- FroshPerformance.php
+++ FroshPerformance.php
@@ -9,6 +9,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 use FroshPerformance\Components\CompilerPass\AddTemplatePluginDirCompilerPass;
 use FroshPerformance\Components\CompilerPass\SetCustomTemplateManager;
 use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\InstallContext;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -36,6 +37,13 @@ class FroshPerformance extends Plugin
             );
 
             $loader->load($this->getPath() . '/Resources/services/theme.xml');
+        }
+    }
+
+    public function install(InstallContext $context)
+    {
+        if(!$context->assertMinimumVersion('5.4.0')) {
+            $context->scheduleMessage('ATTENTION! Do not use ' . $this->getName() . ' below SW 5.4.0');
         }
     }
 }
