332832f9833c32431b8c6d30aedbf18394adedfb
Soner Sayakci <s.sayakci@shopware.com>
2019-09-02 14:08:47 +0200

    Removed unneeded features for 5.6

--- FroshPerformance.php
+++ FroshPerformance.php
@@ -2,10 +2,6 @@
 
 namespace FroshPerformance;
 
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require __DIR__ . '/vendor/autoload.php';
-}
-
 use FroshPerformance\Components\CompilerPass\AddTemplatePluginDirCompilerPass;
 use FroshPerformance\Components\CompilerPass\SetCustomTemplateManager;
 use Shopware\Components\Plugin;
@@ -13,29 +9,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-/**
- * Class FroshPerformance
- */
 class FroshPerformance extends Plugin
 {
-    /**
-     * @param ContainerBuilder $container
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-        $container->addCompilerPass(new AddTemplatePluginDirCompilerPass());
-        $container->addCompilerPass(new SetCustomTemplateManager());
-
-        $plugins = $container->getParameter('active_plugins');
-
-        if (!isset($plugins['DneCustomJsCss'])) {
-            $loader = new XmlFileLoader(
-                $container,
-                new FileLocator()
-            );
-
-            $loader->load($this->getPath() . '/Resources/services/theme.xml');
-        }
-    }
 }
